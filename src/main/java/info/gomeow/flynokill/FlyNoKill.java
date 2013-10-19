fdae1c4a788db219345d325d51d4c27ca5d4ff22
gomeow <davismariotti@gmail.com>
2013-10-18 22:14:30 -0700

    Add updater

--- src/main/java/info/gomeow/flynokill/FlyNoKill.java
+++ src/main/java/info/gomeow/flynokill/FlyNoKill.java
@@ -1,5 +1,8 @@
 package info.gomeow.flynokill;
 
+import info.gomeow.flynokill.Updater.UpdateResult;
+import info.gomeow.flynokill.Updater.UpdateType;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,33 +36,38 @@ public class FlyNoKill extends JavaPlugin implements Listener {
     public void onEnable() {
         saveDefaultConfig();
         getServer().getPluginManager().registerEvents(this, this);
-        if(disableFlyOnHit = getConfig().getBoolean("Disable-Fly-On-Hit", false)) {
+        if (disableFlyOnHit = getConfig().getBoolean("Disable-Fly-On-Hit", false)) {
             cooldown = getConfig().getInt("Cooldown", 5) * 20;
         }
         try {
             Metrics metrics = new Metrics(this);
             metrics.start();
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+        if (getConfig().getBoolean("update", true)) {
+            Updater updater = new Updater(this, 46262, getFile(), UpdateType.DEFAULT, true);
+            if (updater.getResult() == UpdateResult.UPDATE_AVAILABLE) {
+                getLogger().info("New version available! " + updater.getLatestName());
+            }
         }
     }
 
     @EventHandler
     public void onDamage(EntityDamageByEntityEvent event) {
         Entity entity = event.getEntity();
-        if(event.getDamager() instanceof Player) {
+        if (event.getDamager() instanceof Player) {
             Player damager = (Player) event.getDamager();
-            if(!damager.hasPermission("flynokill.bypass") && damager.isFlying()) {
+            if (!damager.hasPermission("flynokill.bypass") && damager.isFlying()) {
                 event.setCancelled(true);
                 damager.sendMessage(ChatColor.RED + "You cannot fight while flying!");
             }
 
-        }
-        else if(event.getDamager() instanceof Arrow) {
+        } else if (event.getDamager() instanceof Arrow) {
             Arrow arrow = (Arrow) event.getDamager();
-            if((arrow.getShooter() instanceof Player)) {
+            if ((arrow.getShooter() instanceof Player)) {
                 Player shooter = (Player) arrow.getShooter();
-                if(!shooter.hasPermission("flynokill.bypass") && shooter.isFlying()) {
+                if (!shooter.hasPermission("flynokill.bypass") && shooter.isFlying()) {
                     event.setDamage(0);
                     shooter.sendMessage(ChatColor.RED + "You cannot fight while flying!");
                 }
@@ -68,12 +76,12 @@ public class FlyNoKill extends JavaPlugin implements Listener {
 
         }
 
-        if(disableFlyOnHit) {
-            if(entity instanceof Player) {
+        if (disableFlyOnHit) {
+            if (entity instanceof Player) {
                 final Player damaged = (Player) entity;
-                if(!damaged.hasPermission("flynokill.bypass")) {
-                    if(damaged.isFlying()) {
-                        if(!cooldownPlayers.contains(damaged.getName())) {
+                if (!damaged.hasPermission("flynokill.bypass")) {
+                    if (damaged.isFlying()) {
+                        if (!cooldownPlayers.contains(damaged.getName())) {
                             cooldownPlayers.add(damaged.getName());
                             damaged.setFlying(false);
                             damaged.sendMessage(ChatColor.DARK_RED + "You can't fly for " + (cooldown / 20) + " seconds!");
@@ -94,15 +102,15 @@ public class FlyNoKill extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onThrow(ProjectileLaunchEvent event) {
-        if((event.getEntity() instanceof ThrownPotion)) {
+        if ((event.getEntity() instanceof ThrownPotion)) {
             ThrownPotion potion = (ThrownPotion) event.getEntity();
-            if((potion.getShooter() instanceof Player)) {
+            if ((potion.getShooter() instanceof Player)) {
                 Player p = (Player) potion.getShooter();
-                if(!p.hasPermission("flynokill.bypass")) {
+                if (!p.hasPermission("flynokill.bypass")) {
                     Collection<PotionEffect> effects = potion.getEffects();
-                    if(p.isFlying()) {
-                        for(PotionEffect pe:effects) {
-                            if(potions.contains(pe.getType().getId())) {
+                    if (p.isFlying()) {
+                        for (PotionEffect pe : effects) {
+                            if (potions.contains(pe.getType().getId())) {
                                 event.setCancelled(true);
                                 p.sendMessage(ChatColor.RED + "You cannot fight while flying!");
                                 break;
@@ -117,9 +125,9 @@ public class FlyNoKill extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player p = event.getPlayer();
-        if(p.getItemInHand().getType() == Material.FLINT_AND_STEEL) {
-            if(!p.hasPermission("flynokill.bypass")) {
-                if(p.isFlying()) {
+        if (p.getItemInHand().getType() == Material.FLINT_AND_STEEL) {
+            if (!p.hasPermission("flynokill.bypass")) {
+                if (p.isFlying()) {
                     event.setCancelled(true);
                 }
             }
@@ -129,7 +137,7 @@ public class FlyNoKill extends JavaPlugin implements Listener {
     @EventHandler
     public void onFlyChange(PlayerToggleFlightEvent event) {
         Player p = event.getPlayer();
-        if(cooldownPlayers.contains(p.getName()))
+        if (cooldownPlayers.contains(p.getName()))
             event.setCancelled(true);
     }
 }
